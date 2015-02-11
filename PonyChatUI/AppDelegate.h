1538d4d0d88e804ef2794bdb932d5f6cba4dc6f1
PonyCui <cuiminghui@yy.com>
2015-02-11 11:21:34 +0800

    Initial Commit

---
+++ PonyChatUI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PonyChatUI
+//
+//  Created by 崔 明辉 on 15/2/11.
+//  Copyright (c) 2015年 多玩事业部 iOS开发组 YY Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
